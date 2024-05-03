a31a7963aba2a3f6c72e4899675449617a8904ba
Giacomo <jackrosilho@gmail.com>
2024-05-03 18:28:59 +0200

    bla

--- pySDC/projects/Monodomain/run_scripts/run_stability_experiment.py
+++ pySDC/projects/Monodomain/run_scripts/run_stability_experiment.py
@@ -1,6 +1,8 @@
 import os
-import numpy as np
-from pySDC.projects.Monodomain.run_scripts.run_MonodomainODE_cli_slurm_wrapper import execute_with_dependencies, options_command
+from pySDC.projects.Monodomain.run_scripts.run_MonodomainODE_cli_slurm_wrapper import (
+    execute_with_dependencies,
+    options_command,
+)
 
 
 def main():
@@ -13,7 +15,7 @@ def main():
     # define sweeper parameters
     options = dict()
     options["integrator"] = "IMEXEXP_EXPRK"
-    options["num_nodes"] = [8, 4]
+    options["num_nodes"] = [8, 4, 2, 1]
     options["num_sweeps"] = [1]
 
     # set step parameters
@@ -24,7 +26,7 @@ def main():
 
     # set level parameters
     # options["dt"] = 0.2
-    dt_list = [0.0125, 0.025, 0.05, 0.1, 0.2]
+    dt_list = [0.0125, 0.025, 0.05, 0.1]  # , 0.2]
     options["restol"] = 5e-8
 
     # set time parallelism to True or emulated (False)
@@ -52,7 +54,7 @@ def main():
 
     slurm_options["dry_run"] = False
     slurm_options["overwrite_existing_results"] = False
-    slurm_options["ntaskspernode"] = 8 if slurm_options["cluster"] == "eiger" else 12
+    slurm_options["ntaskspernode"] = 4 if slurm_options["cluster"] == "eiger" else 12
     minutes = 0
     hours = 4
     slurm_options["run_time"] = 3600 * hours + 60 * minutes
@@ -61,7 +63,9 @@ def main():
     job_number = 0
 
     base_python_command = "python3 run_MonodomainODE_cli.py"
-    local_docker_command = "docker exec -w /src/pySDC/pySDC/projects/Monodomain/run_scripts -it my_dolfinx_daint_container_monodomain_new "
+    local_docker_command = (
+        "docker exec -w /src/pySDC/pySDC/projects/Monodomain/run_scripts -it my_dolfinx_daint_container_monodomain_new "
+    )
 
     for ionic_model_name in ionic_models_list:
         options["ionic_model_name"] = ionic_model_name
@@ -73,11 +77,24 @@ def main():
                 num_nodes_str = "-".join([str(num_node) for num_node in options["num_nodes"]])
                 pre_refinements_str = "-".join([str(pre_refinement) for pre_refinement in options["pre_refinements"]])
                 options["output_file_name"] = (
-                    "pre_refinements_" + pre_refinements_str + "_num_nodes_" + num_nodes_str + "_dt_" + str(options["dt"]).replace(".", "p") + "_n_time_ranks_" + str(n_time_ranks)
+                    "pre_refinements_"
+                    + pre_refinements_str
+                    + "_num_nodes_"
+                    + num_nodes_str
+                    + "_dt_"
+                    + str(options["dt"]).replace(".", "p")
+                    + "_n_time_ranks_"
+                    + str(n_time_ranks)
                 )
                 n_tasks = n_space_ranks * n_time_ranks
                 if local:
-                    os.system(local_docker_command + f"mpirun -n {n_tasks} " + base_python_command + " " + options_command(options))
+                    os.system(
+                        local_docker_command
+                        + f"mpirun -n {n_tasks} "
+                        + base_python_command
+                        + " "
+                        + options_command(options)
+                    )
                 else:
                     slurm_options["n_tasks"] = n_tasks
                     merged_opts = options.copy()
